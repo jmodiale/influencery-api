77a04d444c2dc005275afe8908747f8006446c8d
remen <remen.omodiale@outlook.com>
2022-04-01 16:14:45 +0100

    created migrate file and added associations

--- app/controllers/api/v1/influencers_controller.rb
+++ app/controllers/api/v1/influencers_controller.rb
@@ -1,22 +1,6 @@
 class Api::V1::InfluencersController < ApplicationController
     def index
-        @influencers= Influencer.includes(:platform, :tags).all
+        @influencers= Influencer.includes(:platform, :tags, :primary_tag).all
         render json: @influencers
     end
-
-    def create
-        @influencer = Influencer.new(influencer_params)
-
-        if @influencer.save
-            render json: @influencer, status: :created
-        else
-            render json: @influencer.errors, status: :unprocessable_entity
-        end
-    end
-
-    private
-    def influencer_params
-        params.require(:influencer).permit(:handle, :followers, :platform_id, :profile_pic_url)
-    end
-
 end
--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,3 +1,6 @@
 class Influencer < ApplicationRecord
   belongs_to :platform
+  has_many :influencer_tags
+  has_many :tags, through: :influencer_tags
+  belongs_to :primary_tag, class_name: 'Tag'
 end
