b1a8c2a46524d42bebd2a35d5866558f961d37ee
remen <remen.omodiale@outlook.com>
2022-04-01 15:26:59 +0100

    controller update

--- app/controllers/api/v1/influencers_controller.rb
+++ app/controllers/api/v1/influencers_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::InfluencersController < ApplicationController
+    def index
+        @influencers= Influencer.includes(:platform, :tags).all
+        render json: @influencers
+    end
+
+    def create
+        @influencer = Influencer.new(influencer_params)
+
+        if @influencer.save
+            render json: @influencer, status: :created
+        else
+            render json: @influencer.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def influencer_params
+        params.require(:influencer).permit(:handle, :followers, :platform_id, :profile_pic_url)
+    end
 
 end
